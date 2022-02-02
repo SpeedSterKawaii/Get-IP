aff29a346415f6a80bf01d2a29bf2b73af8394fd
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2022-02-02 09:15:08 -0500

    Create get ip.c

---
+++ get ip.c
@@ -0,0 +1,11 @@
+#include<conio.h>
+#include<stdlib.h>
+void main()
+{
+   clrscr();
+
+   system("C:\\Windows\\System32\\ipconfig");
+
+   getch();
+   Sleep(0404040);
+}//dll only to make to console change void to int and put return NULL; at the botto of sleep()
